d65e3d06dcd0baec7275484d2090388d1f16a1b8
vvp117 <ponomarev117@gmail.com>
2021-09-22 22:11:14 +0700

    feat(devices): add CRUD for Devices

--- backend/apps/devices/models.py
+++ backend/apps/devices/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
+
 
 class Device(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -11,3 +13,6 @@ class Device(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('devices-item', kwargs={"pk": self.pk})
--- backend/apps/devices/urls.py
+++ backend/apps/devices/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.devices_list, name='devices-list'),
+    path('',
+         views.DeviceListView.as_view(), name='devices-list'),
+    path('<int:pk>/',
+         views.DeviceDetailView.as_view(), name='devices-item'),
+    path('new/',
+         views.DeviceCreateView.as_view(), name='devices-new'),
+    path('<int:pk>/edit/',
+         views.DeviceUpdateView.as_view(), name='devices-edit'),
+    path('<int:pk>/del/',
+         views.DeviceDeleteView.as_view(), name='devices-del'),
 ]
